379774f01e3efbad574300cb5013aa8eb8d1e172
Frontasts <steamfronts0110@gmail.com>
2026-09-08 14:07:53 +0300

    Добавлен абстрактный класс карточки
    Требуется для дальнейшей реализации текстовых карточек, звуковых карточек и карточек с изображением

---
+++ QuizGame/Models/Card.cs
@@ -0,0 +1,45 @@
+﻿namespace QuizGame.Models
+{
+    public abstract class Card
+    {
+        private string _name;
+        private int _point;
+        private string _answer;
+        private bool _isPassed;
+
+        public Card(string name, int point, string answer)
+        {
+            _name = name;
+            _point = point;
+            _isPassed = false;
+            _answer = answer;
+        }
+
+        public bool TrySetIsPassed(bool isPassed)
+        {
+            _isPassed = isPassed;
+
+            return _isPassed;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public int GetPoint()
+        {
+            return _point;
+        }
+
+        public string GetAnswer()
+        {
+            return _answer;
+        }
+
+        public bool GetIsPassed()
+        {
+            return _isPassed;
+        }
+    }
+}
