cb32873b26ec85f6997f16cc673c8c82b98272d6
Frontasts <steamfronts0110@gmail.com>
2026-09-08 14:18:19 +0300

    Добавлен вспомогательный метод PrintCenter

--- QuizGame/UI/UI.cs
+++ QuizGame/UI/UI.cs
@@ -1,6 +1,20 @@
-﻿namespace QuizGame.UI
+﻿using System;
+
+namespace QuizGame.UI
 {
     public static class UI
     {
+        private static void PrintCenter(string text, int rowOffset = 0)
+        {
+            const int Modifier = 2;
+
+            int left = Math.Max(0, (Console.WindowWidth - text.Length) / Modifier);
+            int top = Math.Max(0, Console.WindowHeight / Modifier + rowOffset);
+
+            Console.SetCursorPosition(left, top);
+            Console.WriteLine(text);
+
+            Console.SetCursorPosition(left, top + 1);
+        }
     }
 }
