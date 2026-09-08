3d90e461bf8f2fa7d812faef1487f865d21b8e5d
Frontasts <steamfronts0110@gmail.com>
2026-09-08 14:12:50 +0300

    Добавлен класс текстовой карточки
    Требуется для реализации карточек с текстом

---
+++ QuizGame/Models/TextCard.cs
@@ -0,0 +1,17 @@
+﻿namespace QuizGame.Models
+{
+    public class TextCard : Card
+    {
+        private string _text;
+
+        public TextCard(string name, int point, string answer, string text) : base(name, point, answer)
+        {
+            _text = text;
+        }
+
+        public string GetText()
+        {
+            return _text;
+        }
+    }
+}
