780591838aabbd9d748985f66ded4981e4198c9f
Frontasts <steamfronts0110@gmail.com>
2026-09-08 15:35:14 +0300

    Добавлен метод PrintSoloGame с константными символами
    Требуется для вывода основного "окна" игры в одиночном режиме

--- QuizGame/UI/UI.cs
+++ QuizGame/UI/UI.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using QuizGame.Models;
+using System;
 
 namespace QuizGame.UI
 {
     public static class UI
     {
+        private const char WindowBorderSymbol = '=';
+        private const char EmptySymbol = ' ';
+
         public static void PrintMenu()
         {
             string[] menuTexts = new string[]{
@@ -28,6 +32,65 @@ namespace QuizGame.UI
             {
                 PrintCenter(text, rowOffset);
                 rowOffset ++;
+            }
+        }
+
+        public static void PrintSoloGame(Player player)
+        {
+            const int Width = 22;
+            const int WidthOffset = 2;
+
+            string playerName = player.GetName();
+            string playerPoint = player.GetPoint().ToString();
+
+            int playerNameLength = playerName.Length;
+            int playerPointLength = playerPoint.Length;
+
+            int firstWindowBorderWidth = Width + playerNameLength + playerPointLength + WidthOffset;
+            int secondWindowBorderWidth = Width + playerNameLength + playerPointLength;
+
+            string[] header = new string[]{
+                new string(WindowBorderSymbol, firstWindowBorderWidth),
+                $"{WindowBorderSymbol} {playerName}             очки - {playerPoint} {WindowBorderSymbol}",
+                new string(WindowBorderSymbol, firstWindowBorderWidth)
+            };
+
+            string[] main = new string[]{
+                WindowBorderSymbol + new string(EmptySymbol, secondWindowBorderWidth) + WindowBorderSymbol,
+                WindowBorderSymbol + new string(EmptySymbol, secondWindowBorderWidth) + WindowBorderSymbol,
+                WindowBorderSymbol + new string(EmptySymbol, secondWindowBorderWidth) + WindowBorderSymbol,
+                WindowBorderSymbol + new string(EmptySymbol, secondWindowBorderWidth) + WindowBorderSymbol,
+                WindowBorderSymbol + new string(EmptySymbol, secondWindowBorderWidth) + WindowBorderSymbol,
+                WindowBorderSymbol + new string(EmptySymbol, secondWindowBorderWidth) + WindowBorderSymbol,
+                WindowBorderSymbol + new string(EmptySymbol, secondWindowBorderWidth) + WindowBorderSymbol,
+                WindowBorderSymbol + new string(EmptySymbol, secondWindowBorderWidth) + WindowBorderSymbol
+            };
+
+
+            string[] footer = new string[]{
+                new string(WindowBorderSymbol, firstWindowBorderWidth),
+                WindowBorderSymbol + new string(EmptySymbol, secondWindowBorderWidth) + WindowBorderSymbol,
+                new string(WindowBorderSymbol, firstWindowBorderWidth)
+            };
+
+            int rowOffset = -15;
+
+            foreach (string text in header)
+            {
+                PrintCenter(text, rowOffset);
+                rowOffset++;
+            }
+
+            foreach (string text in main)
+            {
+                PrintCenter(text, rowOffset);
+                rowOffset++;
+            }
+
+            foreach (string text in footer)
+            {
+                PrintCenter(text, rowOffset);
+                rowOffset++;
             }
         }
 
