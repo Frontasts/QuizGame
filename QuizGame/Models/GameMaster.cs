2a95b556be0ef96ba008ad24d2b9a1eda46006bc
Frontasts <steamfronts0110@gmail.com>
2026-09-08 13:38:45 +0300

    Добавлена сущность мастера игры
    Требуется для реализации мастера игры, который будет координировать игру в многопользовательском режиме

---
+++ QuizGame/Models/GameMaster.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame.Models
+{
+    public static class GameMaster
+    {
+        private static string _name;
+        private static bool _isActive = false;
+
+        public static bool TrySetName(string name)
+        {
+            if (string.IsNullOrEmpty(_name) == false)
+            {
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(name))
+            {
+                return false ;
+            }
+
+            _name = name;
+            return true;
+        }
+
+        public static bool SetIsActive(bool isActive)
+        {
+            _isActive = isActive;
+
+            return _isActive;
+        }
+
+        public static string GetName()
+        {
+            return _name;
+        }
+
+        public static bool GetIsActive()
+        {
+            return _isActive;
+        }
+    }
+}
