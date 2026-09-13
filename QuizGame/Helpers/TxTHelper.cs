141fff4e614f25e58bad82c47f9eb12dd5c6000a
Frontasts <steamfronts0110@gmail.com>
2026-09-13 12:55:58 +0300

    Создан класс TxTHelper с методами для чтения и записи в txt файлы

---
+++ QuizGame/Helpers/TxTHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame.Helpers
+{
+    public static class TxTHelper
+    {
+        public static bool TxTWrite(string filePath, string[] texts)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                return false;
+            }
+            else if (File.Exists(filePath) == false)
+            {
+                return false;
+            }
+
+            File.WriteAllLines(filePath, texts, Encoding.UTF8);
+            return true;
+        }
+
+        public static string[] TxTRead(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                return null;
+            }
+            else if (File.Exists(filePath) == false)
+            {
+                return null;
+            }
+
+            string[] text = File.ReadAllLines(filePath, Encoding.UTF8);
+            return text;
+        }
+    }
+}
