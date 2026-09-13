3fefa2b5794fa9850fd1bda4edacda054f4c9187
Frontasts <steamfronts0110@gmail.com>
2026-09-13 12:19:20 +0300

    Созданна базовая логика главного меню

--- QuizGame/Game.cs
+++ QuizGame/Game.cs
@@ -10,7 +10,40 @@ namespace QuizGame
     {
         public void Run()
         {
+            const char SoloGameCommand = '1';
+            const char LocalGameCommand = '2';
+            const char MultiplayerCommand = '3';
+            const char OptionsCommand = '4';
+            const char ExitCommand = '0';
 
+            UI.UI.PrintMenu();
+
+            bool isRunGame = true;
+            
+            while (isRunGame)
+            {
+                char key = Console.ReadKey().KeyChar;
+
+                switch (key)
+                {
+                    case SoloGameCommand:
+                        break;
+
+                    case LocalGameCommand:
+                        break;
+
+                    case MultiplayerCommand:
+                        break;
+
+                    case OptionsCommand:
+                        break;
+
+                    case ExitCommand:
+                        isRunGame = false;
+                        break;
+
+                }
+            }
         }
     }
 }
